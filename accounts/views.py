783bbbc9ebe6862e11bc2a16ea5a9e2444d95e52
Tsi2216 <getanehshimelis40@gmail.com>
2025-10-21 06:46:47 -0700

    modified file

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 # accounts/views.py
-from rest_framework import viewsets, mixins
+
+from rest_framework import viewsets, mixins, permissions # <-- IMPORT permissions
 from .serializers import UserSerializer, RegisterSerializer
 from django.contrib.auth import get_user_model
 
@@ -14,6 +15,27 @@ class UserViewSet(
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    # -----------------------------------------------------------
+    # FIX: Override get_permissions to allow anonymous registration
+    # -----------------------------------------------------------
+    def get_permissions(self):
+        """
+        Sets the permissions based on the requested action.
+        Allows anonymous users for the 'create' (POST/registration) action.
+        """
+        if self.action == 'create':
+            # Allow ANY user (anonymous or authenticated) to register
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            # Require authentication for all other actions (retrieve, update, etc.)
+            self.permission_classes = [permissions.IsAuthenticated]
+        
+        # Instantiate and return the permission classes
+        return [permission() for permission in self.permission_classes]
+
+    # -----------------------------------------------------------
+    # Original get_serializer_class method
+    # -----------------------------------------------------------
     def get_serializer_class(self):
         if self.action == 'create':
             return RegisterSerializer
